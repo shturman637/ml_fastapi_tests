5b0c5b1b06f3f4469e77e7503f8c6856c5cf8fd9
Sergey Sokolov <152925976+shturman637@users.noreply.github.com>
2024-06-23 22:27:35 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,26 +1,34 @@
 from fastapi.testclient import TestClient
-from main import app
+from fast_api_app import app
+
 
 client = TestClient(app)
 
 
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "World"}
-
-
-def test_predict_positive():
-    response = client.post("/predict/",
+def test_predict2_positive():
+    response = client.post("/predict2/",
                            json={"text": "I like machine learning!"})
     json_data = response.json()
     assert response.status_code == 200
     assert json_data['label'] == 'POSITIVE'
 
 
-def test_predict_negative():
-    response = client.post("/predict/",
+def test_predict2_negative():
+    response = client.post("/predict2/",
                            json={"text": "I hate machine learning!"})
     json_data = response.json()
     assert response.status_code == 200
     assert json_data['label'] == 'NEGATIVE'
+
+def test_predict_correct_answer():
+    response = client.post("/predict/",
+                           json={"text": "I hate machine learning!"})
+    assert response.status_code == 200
+
+def test_predict_not_empty_answer():
+    response = client.post("/predict/",
+                           json={"text": "I hate machine learning!"})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert (len(json_data)) >= 1
+    
